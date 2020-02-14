d1aa6e6ae076736cb97f24193e5760e9f70de342
Diana Grams <deegrams221@gmail.com>
2020-02-14 09:53:43 -0700

    added notes to ex2.py

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -5,7 +5,11 @@ from hashtables import (HashTable,
                         hash_table_retrieve,
                         hash_table_resize)
 
-
+"""
+* The crux of this problem requires us to 'link' tickets together to reconstruct the entire trip. For example, if we have a ticket `('SJC', 'BOS')` that has us flying from San Jose to Boston, then there exists another ticket where Boston is the starting location, `('BOS', 'JFK')`.
+* We can hash each ticket such that the starting location is the key and the destination is the value. Then, when constructing the entire route, the `i`th location in the route can be found by checking the hash table for the `i-1`th location.
+* You might need to do some cleanup at the end to make sure results match what the test and example are expecting.
+"""
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
@@ -19,5 +23,15 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    # print tickets and length to check the data
 
-    pass
+    # for loop - ticket in tickets (key: source, value: destination)
+        # hash table insert function ht, source, and destination
+    # set 1st route equal to retrieve function ht, and 'None' source
+
+    # for loop - index in range of 1, length (skip source)
+        # set index route equal to retrieve function ht, route index - 1
+
+    # return route
+
+    # test tickets
